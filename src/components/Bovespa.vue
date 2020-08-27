eb85388fe9d9c818de6d4f55259e000d55040926
Lívia Delgado <livia.carneiro@animaeducacao.com.br>
2020-08-26 21:30:08 -0300

    feat: add api connection to bovespa component

--- src/components/Bovespa.vue
+++ src/components/Bovespa.vue
@@ -9,40 +9,41 @@
           <v-expansion-panel-content>
             <v-card class="ma-0 pa-0">
               <v-card-text>
-                <v-layout align-end justify-center pa-0 mt-0 mb-4>
-                  <span>BOVV11.SAO</span>
+                <v-layout align-end justify-space-around pa-0 mt-0 mb-4>
+                  <span>{{ this.bovespa.simbolo }}</span>
                 </v-layout>
-                <v-layout row wrap pl-3 justify-space-around align-center class="d-flex">
-                  <v-flex class="text-center">
+                <v-layout row wrap justify-space-around align-center class="d-flex">
+                  <v-flex xs2 class="text-center">
                     <p>Pontos</p>
-                    <h3>102.117</h3>
+                    <h3>{{ formatValue(this.atualizacaoRecente.close_value) }}</h3>
                   </v-flex>
-                  <v-flex class="text-center">
+                  <v-flex xs2 class="text-center">
                     <p>Volume</p>
-                    <h3>102.117</h3>
+                    <h3>{{ this.atualizacaoRecente.volume_value }}</h3>
                   </v-flex>
-                  <v-flex class="text-center">
+                  <v-flex xs2 class="text-center">
                     <p>Máximo (Dia)</p>
-                    <h3>102.117</h3>
+                    <h3>{{ formatValue(this.atualizacaoRecente.high_value) }}</h3>
                   </v-flex>
-                  <v-flex class="text-center">
+                  <v-flex xs2 class="text-center">
                     <p>Mínimo (Dia)</p>
-                    <h3>102.117</h3>
+                    <h3>{{ formatValue(this.atualizacaoRecente.low_value) }}</h3>
                   </v-flex>
-                  <v-flex class="text-center">
+                  <v-flex xs2 class="text-center">
                     <p>Variação</p>
-                    <h3>-0.18%</h3>
+                    <h3>{{ this.variacao }}</h3>
                   </v-flex>
                 </v-layout>
               </v-card-text>
               <v-layout align-end justify-center pa-0 mt-0 mb-4>
-                <span>Última atualização em: 25/08/2020</span>
+                <span class="caption">Última atualização em: 25/08/2020</span>
               </v-layout>
             </v-card>
             <v-sheet color="#1C1C1C">
               <v-sparkline
                 :value="this.valoresGraficoBovespa"
                 :labels="this.labelsGraficoBovespa"
+                :line-width="2"
                 color="#088A08"
                 height="100"
                 padding="24"
@@ -58,75 +59,67 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
-  data: () => ({
-    atualizacoes: [
-      {
-        data: "2020-08-25",
-        open_value: "103.0000",
-        high_value: "103.4300",
-        low_value: "102.0400",
-        close_value: "102.5300",
-        volume_value: "1364900",
-      },
-      {
-        data: "2020-08-24",
-        open_value: "101.8200",
-        high_value: "102.9400",
-        low_value: "101.8200",
-        close_value: "102.6800",
-        volume_value: "1065760",
-      },
-      {
-        data: "2020-08-21",
-        open_value: "102.0000",
-        high_value: "102.0600",
-        low_value: "100.8100",
-        close_value: "101.8000",
-        volume_value: "1392930",
-      },
-      {
-        data: "2020-08-20",
-        open_value: "99.9500",
-        high_value: "102.2500",
-        low_value: "99.3200",
-        close_value: "102.0000",
-        volume_value: "1373510",
-      },
-      {
-        data: "2020-08-19",
-        open_value: "102.4000",
-        high_value: "102.6600",
-        low_value: "101.1500",
-        close_value: "101.2800",
-        volume_value: "1729650",
-      },
-      {
-        data: "2020-08-18",
-        open_value: "102.0000",
-        high_value: "102.7700",
-        low_value: "101.5500",
-        close_value: "102.4000",
-        volume_value: "1677750",
-      },
-    ],
-  }),
+  data: () => ({}),
   computed: {
+    ...mapGetters(["bovespa"]),
+    atualizacaoRecente() {
+      return this.bovespa.atualizacoes[0];
+    },
     valoresGraficoBovespa() {
-      return this.atualizacoes
+      return this.bovespa.atualizacoes
         .map((atualizacao) => parseInt(atualizacao.high_value))
-        .slice(0, 30)
+        .slice(0, 10)
         .reverse();
     },
     labelsGraficoBovespa() {
-      return this.atualizacoes
+      return this.bovespa.atualizacoes
         .map((atualizacao) => {
           let dataSplitted = atualizacao.data.split("-");
           return `${dataSplitted[2]}/${dataSplitted[1]}`;
         })
-        .slice(0, 30)
+        .slice(0, 10)
         .reverse();
     },
+    variacao() {
+      if (this.bovespa.atualizacoes[1]) {
+        let atualizacaoHoje = this.formatValue(
+          this.bovespa.atualizacoes[0].close_value
+        );
+        let atualizacaoOntem = this.formatValue(
+          this.bovespa.atualizacoes[1].close_value
+        );
+        let caractere = "";
+        let valorVariacao = 0;
+        if (atualizacaoHoje > atualizacaoOntem) {
+          caractere = "+";
+          valorVariacao =
+            ((atualizacaoHoje - atualizacaoOntem) * 100) / atualizacaoOntem;
+        } else {
+          caractere = "-";
+          valorVariacao =
+            ((atualizacaoOntem - atualizacaoHoje) * 100) / atualizacaoOntem;
+        }
+        return `${caractere} ${valorVariacao.toFixed(2)}%`;
+      } else {
+        return "-";
+      }
+    },
+  },
+  methods: {
+    ...mapActions(["obterDadosBovespa"]),
+    formatValue(value) {
+      value = value.replace(".", "");
+      let formattedValue = `${value.slice(0, -1)}.${value.charAt(
+        value.length - 1
+      )}`;
+      return parseFloat(formattedValue).toFixed(2);
+    },
+  },
+  mounted: async function () {
+    await this.obterDadosBovespa();
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,26 @@
 import Vuex from "vuex";
 import Vue from "vue";
 
+import services from '../services';
+
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {},
-    getters: {},
-    actions: {},
-    mutations: {}
+    state: {
+        bovespa: null
+    },
+    getters: {
+        bovespa: state => state.bovespa,
+    },
+    actions: {
+        async obterDadosBovespa({ commit }) {
+            let bovespa = await services.obterDadosBovespa();
+            commit("setBovespa", bovespa.data, { root: true })
+        }
+    },
+    mutations: {
+        setBovespa: (state, bovespa) => state.bovespa = bovespa,
+    }
 });
+
+export default store;
