01b46aa7f45c65d0a75dae18e951e07fb586b66b
Lívia Delgado <livia.carneiro@animaeducacao.com.br>
2020-08-27 21:44:59 -0300

    fix axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -6,8 +6,7 @@ export default () => {
             axios.create({
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
-                // baseURL: "https://bolsa-de-valores-livia-delgado.herokuapp.com/",
-                baseURL: "http://127.0.0.1:8000/"
+                baseURL: "https://bolsa-de-valores-livia-delgado.herokuapp.com/",
             }),
     };
 };
